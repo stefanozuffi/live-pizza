914d0c5966f252e3c4129e0260be9328d687a2c1
stefanozuffi <stefano.zuffi06@outlook.com>
2025-09-04 20:42:27 +0200

    Aggiunta degli ID alle pizze e modifica delle risposte delle API per restituire dati in formato JSON + route show

--- index.js
+++ index.js
@@ -17,22 +17,28 @@ app.get('/', (req, res) => {
 
 const menu = [
     {
+      id: 1,
       name: "Margherita",
       image: "imgs/margherita.webp",
       ingredients: ["pomodoro", "mozzarella"],
-    }, {
+    }, 
+    {
+      id: 2,
       name: "Marinara",
       image: "imgs/marinara.jpeg",
       ingredients: ["pomodoro", "aglio", "origano"],
     }, {
+      id: 3,
       name: "Diavola",
       image: "imgs/diavola.jpeg",
       ingredients: ["pomodoro", "mozzarella", "salame piccante"],
     }, {
+      id: 4,
       name: "Bufalina",
       image: "imgs/bufalina.jpeg",
       ingredients: ["pomodoro", "mozzarella di bufala"],
     }, {
+      id: 5,
       name: "4 formaggi",
       image: "imgs/4_formaggi.jpeg",
       ingredients: ["pomodoro", "mozzarella", "gorgonzola", "parmigiano", "ricotta"],
@@ -42,31 +48,40 @@ const menu = [
 //CRUD operations
 
 //Index
-app.get('/api/pizzas', (req,res) => {
-    res.send('Show a list of pizzas')
+app.get('/api/pizzas', (req, res) => {
+    res.json(menu)
 })
 
 //Show
 app.get('/api/pizzas/:id', (req, res) => {
-    res.send('Show single pizza with id' + req.params.id)
+    const pizzaId = parseInt(req.params.id)
+    const pizza = menu.find(p => p.id === pizzaId)
+    if (pizza) {
+        res.json(pizza)
+    } else {
+        res.status(404).json({
+            error: true,
+            message: 'Pizza Not Found'
+        })
+    }
 })
 
 //Store
-app.post('/api/pizzas', (req,res)=> {
+app.post('/api/pizzas', (req, res)=> {
     res.send('Create a new pizza')
 })
 
 //Update
-app.put('/api/pizzas/:id', (req,res)=> {
+app.put('/api/pizzas/:id', (req, res)=> {
     res.send('Update the single pizza with id ' + req.params.id)
 })
 
 //Modify
-app.patch('api/pizzas/:id', (req,res)=> {
+app.patch('/api/pizzas/:id', (req, res)=> {
     res.send('Partial update the single pizza with id ' + req.params.id)
 })
 
 //Destroy
-app.delete('/api/pizzas/:id', (req,res)=> {
+app.delete('/api/pizzas/:id', (req, res)=> {
     res.send('Delete the single pizza with id ' + req.params.id)
 })
